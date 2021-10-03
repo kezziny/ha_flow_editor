90f56b54f836729b6064f72bd3f140cf8bd8fb35
Kezziny <ferenc.klein.pe@gmail.com>
2021-10-03 19:50:26 +0200

    cnt

--- flow/mqtt_sensor.js
+++ flow/mqtt_sensor.js
@@ -7,12 +7,54 @@ exports.icon = 'sign-in';
 exports.output = 1;
 exports.variables = true;
 exports.author = 'Martin Smola';
-exports.options = {};
+exports.options = { conditions: [{ operator: '==', datatype: 'Number', value: 1, index: 0 }] };
 
-exports.html = `<div class="padding">
+exports.html = `
+<style>
+	.cond-col1 { width:20px; float:left; }
+	.cond-col2 { width:150px; float:left; }
+	.cond-col3 { width:110px; float:left; }
+	.cond-col4 { width:420px; float:left; }
+	.cond-col5 { width:30px; float:left; }
+	.pr10 { padding-right:10px; }
+	.cond-remove { padding: 8px 13px; }
+</style>
+<div class="padding">
 	<div data-jc="dropdown" data-jc-path="broker" data-jc-config="datasource:mqttconfig.brokers;required:true" class="m">@(Select a broker)</div>
 	<div data-jc="dropdown" data-bind="null__click:device_changed" data-jc-path="device" data-jc-config="datasource:mqttconfig.devices;required:true" class="m">@(Select a device)</div>
 	<div data-jc="dropdown" data-jc-path="attribute" data-jc-config="datasource:mqttconfig.attributes;required:true" class="m">@(Select an attribute)</div>
+	<section>
+		<label><i class="fa fa-edit"></i>@(Conditions)</label>
+		<div class="padding npb">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="cond-col1"><strong>#</strong></div>
+					<div class="cond-col2"><strong>Operator</strong></div>
+					<div class="cond-col3"><strong>Data-type</strong></div>
+					<div class="cond-col4"><strong>Value</strong></div>
+				</div>
+			</div>
+			<div data-jc="repeater" data-jc-path="conditions" class="mt10">
+				<script type="text/html">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="cond-col1 mt5"><strong>$index</strong></div>
+						<div class="cond-col2 pr10"><div data-jc="dropdown" data-jc-path="conditions[$index].operator" data-jc-config="items:,>|>,<|<,>=|>=,<=|<=,==|==,!==|!==,startsWith (for strings only)|startsWith,endsWith (for strings only)|endsWith,indexOf|indexOf,Regex (for strings only)|Regex" class="m"></div></div>
+						<div class="cond-col3 pr10"><div data-jc="dropdown" data-jc-path="conditions[$index].datatype" data-jc-config="items:,Number,String,Boolean"></div></div>
+						<div class="cond-col4 pr10"><div data-jc="textbox" data-jc-path="conditions[$index].value" data-jc-config="placeholder:@(enter value)"></div></div>
+						<div class="cond-col5"><button class="exec button button-small cond-remove" data-exec="FUNC.switchcomponent_remove_condition" data-index="$index"><i class="fa fa-trash"></i></button></div>
+					</div>
+				</div>
+				</script>
+			</div>
+			<div class="row">
+				<div class="col-md-2 m">
+					<br>
+					<button class="exec button button-small" data-exec="FUNC.switchcomponent_add_condition"><i class="fa fa-plus mr5"></i>ADD</button>
+				</div>
+			</div>
+		</div>
+	</section>
 </div>
 <script>
 	var mqttconfig = { brokers: [], devices: [], attributes: [] };
@@ -34,6 +76,8 @@ exports.html = `<div class="padding">
     };
 
 	ON('open.mqttsensor', function(component, options) {
+		outputs_count = options.conditions.length || 0;
+
 		opt = options;
 		TRIGGER('mqtt.brokers', 'mqttconfig.brokers');
 		TRIGGER('mqtt.discovery_devices', 'devices');
@@ -44,7 +88,32 @@ exports.html = `<div class="padding">
 	});
 	ON('save.mqttsensor', function(component, options) {
 		!component.name && (component.name = options.broker + (options.topic ? ' -> ' + options.topic : ''));
+		
+		var length = options.conditions.length || 0;
+		if (changed && length !== outputs_count) {
+			if (flow.version < 511) {
+				component.connections = {};
+				setState(MESSAGES.apply);
+			}
+			component.output = length;
+		}
 	});
+
+		var changed = false;
+	var outputs_count;
+
+	FUNC.switchcomponent_add_condition = function() {
+		PUSH('settings.switch.conditions', { operator: '', datatype: '', value: '' });
+		changed = true;
+	};
+
+	FUNC.switchcomponent_remove_condition = function(button) {
+		var index = button.attr('data-index');
+		var conditions = settings.switch.conditions;
+		conditions = conditions.remove('index', parseInt(index));
+		SET('settings.switch.conditions', conditions);
+		changed = true;
+	};
 </script>`;
 
 exports.readme = `
